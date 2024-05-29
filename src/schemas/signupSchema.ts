a1c03113768afe3aff692d14fd5b5a08e9278c1b
Navin grg <grgnabin60@gmail.com>
2024-05-30 01:34:52 +0545

    Bootstrap the app

---
+++ src/schemas/signupSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod"
+
+export const usernameValidation = z
+        .string()
+        .min(5, "Username should have at least 5 characters")
+        .max(25, "Username shouldn't have more than 25 characters")
+        .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters")
+
+export const SignupSchema = z.object({
+    username: usernameValidation,
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z
+                .string()
+                .min(8, "Password must be at least 8 characters")
+                .max(20, "Password shouldn't have more than 20 characters")
+})
